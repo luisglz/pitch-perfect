5a86e5f4c8fa12e414dc1f37daaf34815fbda03b
luisglz <luisglz@gmail.com>
2015-06-04 09:24:45 -0500

    Update RecordSoundsViewController.swift
    Improvements created from Udacity Reviewer's feedback.

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -17,18 +17,13 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     
     var audioRecorder:AVAudioRecorder!
     var recordedAudio: RecordedAudio!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
     
     override func viewWillAppear(animated: Bool) {
         stopButton.hidden = true
         recordButton.enabled = true
         tapToRecord.hidden = false
-        
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
